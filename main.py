3816f94dc9d3c43ecd7de2fd20ffda6c88b4c22b
zachar1a <zacharycg4@gmail.com>
2020-04-19 16:40:43 -0500

    in the stockLog script, I changed it to first check for a dir, then make one if not there and then switch to its path and add the stock files in there so there will be no mess in the main dir and keep it organised

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import stockLog, config
+import config
+import stockLog as sl
 from time import strftime
 
 ticker = input("ticker: ")
@@ -6,6 +7,4 @@ ticker = input("ticker: ")
 currTime = strftime('%H')
 print(currTime)
 
-stockLog.checkForStockFile(ticker, stockLog.retrieveStockData(ticker, config.key))
-
-print(stockLog.retrieveStockData(ticker, config.key))
+sl.checkForStockDir(ticker)
